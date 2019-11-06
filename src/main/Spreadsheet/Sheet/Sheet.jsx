4384585e45c1efb86fc093c87d3c87248ce726ff
Angelo Perera <angelo.perera@anz.com>
2019-11-06 12:34:52 +1100

    Upgrade sheet

--- src/main/Spreadsheet/Sheet/Sheet.jsx
+++ src/main/Spreadsheet/Sheet/Sheet.jsx
@@ -1,87 +1,102 @@
-import React from 'react';
+
+import React, { useState, useEffect, useLayoutEffect, useCallback, useRef } from 'react';
 import { Grid, AutoSizer } from 'react-virtualized';
 import cellRenderer from './cellRenderer';
 import style from './Sheet.scss';
 import * as _ from 'lodash';
 
-class Sheet extends React.Component {
-  state = {
+const Sheet = ({
+  shouldRecomputateGridSize,
+  forceScroll,
+  activeCell,
+  columnCount,
+  columnWidth,
+  getCellValue,
+  onCellChange,
+  onScroll,
+  onCellSelect,
+  onCellMouseOver,
+  onCellDoubleClick,
+  selectedArea,
+  rowCount,
+  rowHeight,
+}) => {
+  const [state, setState] = useState({
     automaticScrollSet: false,
-  }
-  constructor(props) {
-    super(props);
-    this.gridRef = React.createRef();
-  }
+  });
 
-  componentWillReceiveProps(nextProps) {
+  const gridRef = useRef();
+
+  const componentWillReceiveProps = (nextProps) => {
     if (
       (nextProps.forceScroll.vertical !== 0 || nextProps.forceScroll.horizontal !== 0) &&
-      !this.state.automaticScrollSet
+      !state.automaticScrollSet
     ) {
-      this.setState({
+      setState({
+        ...state,
         automaticScrollSet: true,
-      }, this.initiateAutomaticScroll());
+      }, initiateAutomaticScroll());
     }
   }
 
-  componentDidUpdate() {
-    if (this.props.shouldRecomputateGridSize) {
-      this.gridRef.current.recomputeGridSize(); // Does a forceUpdate internally
+  const componentDidUpdate = useEffect(() => {
+    if (shouldRecomputateGridSize) {
+      gridRef.current.recomputeGridSize(); // Does a forceUpdate internally
     } else {
-      this.gridRef.current.forceUpdate(); // Grid will only do a shallow compare of props
+      gridRef.current.forceUpdate(); // Grid will only do a shallow compare of props
     }
-  }
+  });
 
   /**
    * Overrides manual scroll in favour of the values specified in props.forceScroll.
    */
-  initiateAutomaticScroll() {
+  const initiateAutomaticScroll = () => {
     // Set automatic scroll
     const intervalId = window.setInterval(() => {
-      this.gridRef.current._scrollingContainer.scrollLeft += this.props.forceScroll.horizontal;
-      this.gridRef.current._scrollingContainer.scrollTop += this.props.forceScroll.vertical;
-    }, this.props.forceScroll.interval);
+      gridRef.current._scrollingContainer.scrollLeft += forceScroll.horizontal;
+      gridRef.current._scrollingContainer.scrollTop += forceScroll.vertical;
+    }, forceScroll.interval);
 
     // Clear automatic scroll.
     const _this = this;
     document.addEventListener('mouseup', function clearAutomaticScroll() {
       window.clearInterval(intervalId);
       document.removeEventListener('mouseup', clearAutomaticScroll);
-      _this.setState({
+      setState({
+        ...state,
         automaticScrollSet: false,
       });
     });
-  }
+  };
 
-  render() {
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Grid
-            activeCell={this.props.activeCell}
-            className={style.table}
-            columnCount={this.props.columnCount}
-            columnWidth={this.props.columnWidth}
-            cellRenderer={cellRenderer}
-            getCellValue={this.props.getCellValue}
-            height={height}
-            onCellChange={this.props.onCellChange}
-            onScroll={this.props.onScroll}
-            onCellMouseDown={this.props.onCellSelect}
-            onCellMouseOver={this.props.onCellMouseOver}
-            onCellDoubleClick={this.props.onCellDoubleClick}
-            selectedArea={this.props.selectedArea}
-            style={{ outline: 'none' }}
-            scrollingResetTimeInterval={0}
-            ref={this.gridRef}
-            rowCount={this.props.rowCount}
-            rowHeight={this.props.rowHeight}
-            width={width}
-          />
-        )}
-      </AutoSizer>
-    );
-  }
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <Grid
+          activeCell={activeCell}
+          className={style.table}
+          columnCount={columnCount}
+          columnWidth={columnWidth}
+          cellRenderer={cellRenderer}
+          getCellValue={getCellValue}
+          height={height}
+          onCellChange={onCellChange}
+          onScroll={onScroll}
+          onCellMouseDown={onCellSelect}
+          onCellMouseOver={onCellMouseOver}
+          onCellDoubleClick={onCellDoubleClick}
+          selectedArea={selectedArea}
+          style={{ outline: 'none' }}
+          scrollingResetTimeInterval={0}
+          ref={gridRef}
+          rowCount={rowCount}
+          rowHeight={rowHeight}
+          width={width}
+        />
+      )}
+    </AutoSizer>
+  );
 }
+
 
 export default Sheet;
